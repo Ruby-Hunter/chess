0e1170736f5774ff52ccaddfb4a6a9907d16657f
Ruby-Hunter <rex9832@gmail.com>
2025-10-02 14:09:22 -0600

    implemented checkIfSafe function

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -63,34 +63,92 @@ public class ChessBoard {
         }
     }
 
-    // Used for isInCheck function; checks all directions to see if tile is safe
+    // Used for isInCheck function; checks all directions to see if tile is safe; return true if so, false otherwise
     public boolean checkIfSafe(ChessPosition pos, ChessGame.TeamColor team){
         int row = pos.getRow();
         int col = pos.getColumn();
 
+        // Check if enemy king or pawn can reach
         int[][] basicMoves = { {1,0}, {1,-1}, {0,-1}, {-1,-1}, {-1,0}, {-1,1}, {0,1}, {1,1} };
-        for(int[] dir : basicMoves) {
+        for(int[] move : basicMoves){
+            int curRow = row + move[0];
+            int curCol = col + move[1];
+            if((1 <= curRow && curRow <= 8) && (1 <= curCol && curCol <= 8)){
+                ChessPosition newPos = new ChessPosition(curRow, curCol);
+                ChessPiece encounter = getPiece(newPos);
+                if(encounter != null && encounter.getTeamColor() != team){
+                    if(encounter.getPieceType() == ChessPiece.PieceType.KING) {
+                        return false;
+                    }
+                    else if(encounter.getPieceType() == ChessPiece.PieceType.PAWN){
+                        if(team == ChessGame.TeamColor.WHITE && (move[0] == 1) && (move[1] == 1 || move[1] == -1)){
+                            return false;
+                        }
+                        else if(team == ChessGame.TeamColor.BLACK && (move[0] == -1) && (move[1] == 1 || move[1] == -1)){
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+
+        // Check if rook or queen can reach
+        int[][] straightMoves = { {0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        for(int[] dir : straightMoves) {
             int curRow = row + dir[0]; //x
             int curCol = col + dir[1];//y
             while ((1 <= curRow && curRow <= 8) && (1 <= curCol && curCol <= 8)) {
-                ChessPosition newPos = new ChessPosition(curRow, curCol);
-                ChessPiece encounter = getPiece(newPos);
+                ChessPosition curPos = new ChessPosition(curRow, curCol);
+                ChessPiece encounter = getPiece(curPos);
                 if (encounter != null) {
                     if (encounter.getTeamColor() != team){
-
+                        if(encounter.getPieceType() == ChessPiece.PieceType.ROOK || encounter.getPieceType() == ChessPiece.PieceType.QUEEN){
+                            return false;
+                        }
+                        break;
                     }
-                    moves.add(new ChessMove(myPosition, newPos, null));
-                } else { //encountering any piece
-                    if (encounter.getTeamColor() != curPiece.getTeamColor()) {
-                        moves.add(new ChessMove(myPosition, newPos, null));
-                    }
-                    break;
                 }
                 curRow += dir[0]; // these 2 statements add both the x and y, so they can be easily copied
                 curCol += dir[1];
             }
         }
-        return true;
+
+        // Check if bishop or queen can reach
+        int[][] diagMoves = { {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+        for(int[] dir : diagMoves) {
+            int curRow = row + dir[0]; //x
+            int curCol = col + dir[1];//y
+            while ((1 <= curRow && curRow <= 8) && (1 <= curCol && curCol <= 8)) {
+                ChessPosition curPos = new ChessPosition(curRow, curCol);
+                ChessPiece encounter = getPiece(curPos);
+                if (encounter != null) {
+                    if (encounter.getTeamColor() != team){
+                        if(encounter.getPieceType() == ChessPiece.PieceType.BISHOP || encounter.getPieceType() == ChessPiece.PieceType.QUEEN){
+                            return false;
+                        }
+                        break;
+                    }
+                }
+                curRow += dir[0]; // these 2 statements add both the x and y, so they can be easily copied
+                curCol += dir[1];
+            }
+        }
+
+        // Check if enemy king or pawn can reach
+        int[][] knightMoves = { {2,-1}, {1,-2}, {-1,-2}, {-2,-1}, {-2,1}, {-1,2}, {1,2}, {2,1} };
+        for(int[] move : knightMoves){
+            int curRow = row + move[0];
+            int curCol = col + move[1];
+            if((1 <= curRow && curRow <= 8) && (1 <= curCol && curCol <= 8)){
+                ChessPosition newPos = new ChessPosition(curRow, curCol);
+                ChessPiece encounter = getPiece(newPos);
+                if(encounter != null && encounter.getTeamColor() != team && encounter.getPieceType() == ChessPiece.PieceType.KNIGHT){
+                    return false;
+                }
+            }
+        }
+
+        return true; // If it's safe, return true
     }
 
     @Override
--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -88,7 +88,7 @@ public class ChessGame {
      * @return True if the specified team is in check
      */
     public boolean isInCheck(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        return chessBoard.checkIfSafe(kingPositions.get(teamColor), teamColor);
     }
 
     /**
