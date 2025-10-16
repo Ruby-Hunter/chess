12c70914c2e166ced3ca90acb003df0624c6ab07
Ruby-Hunter <rex9832@gmail.com>
2025-10-16 10:47:23 -0600

    Added in exceptions

---
+++ server/src/main/java/service/BadRequestException.java
@@ -0,0 +1,10 @@
+package service;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+    public BadRequestException(String message, Throwable ex) {
+        super(message, ex);
+    }
+}
