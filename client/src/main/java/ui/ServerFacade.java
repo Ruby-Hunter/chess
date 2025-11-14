3fb0c78136307f16c4eb891aabc1eb26f84594a8
Ruby-Hunter <rex9832@gmail.com>
2025-11-14 10:48:06 -0700

    Made facade base methods

--- client/src/main/java/ui/ServerFacade.java
+++ client/src/main/java/ui/ServerFacade.java
@@ -1,7 +1,13 @@
 package ui;
 
-import java.net.http.HttpClient;
+import com.google.gson.Gson;
 import datamodel.*;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashSet;
 
 public class ServerFacade {
     private final HttpClient client = HttpClient.newHttpClient();
@@ -9,9 +15,39 @@ public class ServerFacade {
 
     public ServerFacade(String url){ serverURL = url; }
 
-    public AuthData register(RegisterRequest request){}
+    public AuthData register(UserData user){
+        var request = buildRequest
+    }
 
-    public LoginResult login(LoginRequest request){}
+    public AuthData login(LoginData login){}
 
-    public JoinResult joinGame(JoinRequest request){}
+    public void logout(String authToken){}
+
+    public Integer createGame(String authToken, String gameName){}
+
+    public HashSet<GameData> listGames(String authToken){}
+
+    public void joinGame(String authToken, JoinData joinData){}
+
+    public void clear(){}
+
+    private HttpRequest buildRequest(String method, String path, Object body){
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create(serverURL + path))
+                .method(method, makeRequestBody(body));
+        if(body != null){
+            request.setHeader("Content-Type", "application/json");
+        }
+        return request.build();
+    }
+
+    private HttpRequest.BodyPublisher makeRequestBody(Object body){
+        if(body != null){
+            return HttpRequest.BodyPublishers.ofString(new Gson().toJson(body));
+        } else{
+            return HttpRequest.BodyPublishers.noBody();
+        }
+    }
+
+    private HttpResponse<String> sendRequest()
 }
